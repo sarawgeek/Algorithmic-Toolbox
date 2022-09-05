79070b1776376ec7254f21467607879edabce6a6
sarawgeek <sarawgeek@gmail.com>
2022-09-05 08:02:05 +0530

    Euclidean Algorithm

--- Week-1/EuclidianGCD.cs
+++ Week-1/EuclidianGCD.cs
@@ -10,15 +10,15 @@ namespace Euclidean
             var tokens = input.Split(' ');
             var a = int.Parse(tokens[0]);
             var b = int.Parse(tokens[1]);
-            Console.WriteLine(EuclidianGCD(a,b));
+            Console.WriteLine(EuclideanGCD(a,b));
         }
 
-        static int EuclidianGCD(int a, int b)
+        static int EuclideanGCD(int a, int b)
         {
             if(a == 0) return b;
             if(b == 0) return a;
-            if(a > b) return EuclidianGCD(b, a % b);
-            else return EuclidianGCD(a, b % a);
+            if(a > b) return EuclideanGCD(b, a % b);
+            else return EuclideanGCD(a, b % a);
         }
 
     }
